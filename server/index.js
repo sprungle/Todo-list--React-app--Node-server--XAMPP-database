e7244c83d78c51a5720affbbd5f2b652683c34a0
Anniina K <89989566+sprungle@users.noreply.github.com>
2022-10-17 14:44:31 +0300

    todo task parts 1-4

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+// this one is good to go :)
+// to-do task parts 1-4 from week 6
+// week 7 and parts 5 and 6 continue on a cloned repo
+
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
@@ -11,19 +15,44 @@ app.use(express.urlencoded({extended: false}));
 
 const port = 3001;
 
+//get-header 
 app.get("/",async function (req,res){
+    try {
+        const connection = await mysql.createConnection(config.db)
+        // res.status(200).send('Database connection was made')
+        
+        const [result,] = await connection.execute('select * from task')
     
-try {
-    const connection = await mysql.createConnection(config.db)
-    // res.status(200).send('Database connection was made')
-    const [result,] = await connection.execute('select * from task')
-   
-    if(!result) result=[] //return emty array if there is no data
-    res.status(200).json(result)
+        if(!result) result=[] //return empty array if there is no data
+        res.status(200).json(result)
 
-} catch(err) {
-    // res.status(500).send(err.message)
-    res.status(500).json({error: err.message})
-}
+    } catch(err) {
+        // res.status(500).send(err.message)
+        res.status(500).json({error: err.message})
+    }
 })
+// post-header
+app.post("/new", async function(req, res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        //execute prepared statment
+        const [result,] = await connection.execute('insert into task (description) values (?) ', [req.body.description])
+        res.status(200).json({id:result.insertId})
+    } catch(err){
+        res.status(500).json({error: err.message})
+    }
+})
+// delete-header
+app.delete("/delete/:id", async function(req, res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        //execute prepared statment
+        await connection.execute('delete from task where id = ? ', [req.params.id])
+        res.status(200).json({id:req.params.id})
+    } catch(err){
+        res.status(500).json({error: err.message})
+    }
+})
+
+
 app.listen(port)
--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const URL = 'http://localhost:3001/'
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState('')
 
   useEffect(()=>{ 
     axios.get(URL)
@@ -16,16 +17,53 @@ function App() {
     })
   }, [])
 
+
+  function save(){
+    const json = JSON.stringify({description: task})
+    axios.post(URL + 'new', json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      //convert stringifyed JSON object back to JS-object
+      const addedObject = JSON.parse(json)
+      // add id, returned by the server, to object
+      addedObject.id = response.data.id
+      //update state variable with newly added data
+      setTasks(tasks => [...tasks, addedObject])
+      setTask('')
+    }).catch(error => {
+      alert(error.response.data.error)
+    })
+  }
+  
+  function remove(id){
+    axios.delete(`${URL}delete/${id}`)
+    .then(()=>{
+      const newListWithoutRemoved = task.filter((item)=> item.id !==id)
+      setTasks(newListWithoutRemoved)
+    }).catch (error =>{
+      alert(error.response.data.error)
+    })
+  }
+
   return (
     <div>
       <h3>My task list</h3>
+      <form>
+        <label>Add new task</label>
+        <input value={task} onChange={e => setTask(e.target.value)}/>
+        <button type='button' onClick={save}>Save</button>
+      </form>
       <ol>
         {tasks.map(task => (
-            <li key={task.id}>{task.description}</li>
+            <li key={task.id}>{task.description} <a href="#" onClick={() => remove(task.id)}>Delete</a> </li>
         ))}
       </ol>
     </div>
   );
+
 }
 
 export default App;
